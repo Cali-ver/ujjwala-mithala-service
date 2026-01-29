74f30e2fb45b500b80e218a3a1447323539ad4d2
Vivek karn <vivekkarn749@gmail.com>
2026-01-29 14:55:44 +0530

    Change in UserController/User

--- src/main/java/com/ujjwala/mithala/ujjwala_mithala_service/entity/User.java
+++ src/main/java/com/ujjwala/mithala/ujjwala_mithala_service/entity/User.java
@@ -2,15 +2,22 @@ package com.ujjwala.mithala.ujjwala_mithala_service.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name ="User")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int  id;
+    private Long  id;
     private String Name;
     private String Email;
+    private String password;
     private String PhoneNumber;
 }
--- src/main/java/com/ujjwala/mithala/ujjwala_mithala_service/repo/UserRepo.java
+++ src/main/java/com/ujjwala/mithala/ujjwala_mithala_service/repo/UserRepo.java
@@ -3,5 +3,5 @@ package com.ujjwala.mithala.ujjwala_mithala_service.repo;
 import com.ujjwala.mithala.ujjwala_mithala_service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface User_Repo extends JpaRepository<User, Long> {
+public interface UserRepo extends JpaRepository<User, Long> {
 }
